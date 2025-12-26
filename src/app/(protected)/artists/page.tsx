15eb88d589d5428d9c6c46058d46eed888ff3091
Jordan Bradley <jordanrhysbradley@gmail.com>
2025-12-26 15:17:45 -0500

    render artists and add artist button scaffold

--- src/app/(protected)/artists/page.tsx
+++ src/app/(protected)/artists/page.tsx
@@ -1,22 +1,64 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 import { usePageHeader } from "@/context/PageHeaderContext";
-import { CircularProgress } from "@mui/material";
+import { Card, CardContent, CircularProgress, Button, Grid, Typography } from "@mui/material";
+import AddArtistButton from "@/components/ui/AddArtistButton";
+
+interface Artists {
+  id: string;
+  name: string;
+}
 
 export default function ArtistsPage() {
+  const router = useRouter();
+  const [artists, setArtists] = useState<Artists[]>([])
+  const [loadingArtists, setLoadingArtists] = useState(true);
   const { user, loading } = useAuth();
   const { setPageHeader } = usePageHeader();
 
   useEffect(() => {
     setPageHeader({ title: "Artists" });
+
+    async function loadArtists() {
+      try {
+        const res = await fetch("/api/artists");
+        if (!res.ok) throw new Error("Failed to fetch artists");
+  
+        const data = await res.json();
+        setArtists(data.artists);
+      } catch (err) {
+        console.error("Failed to load artists", err);
+      } finally {
+        setLoadingArtists(false);
+      }
+    }
+
+    loadArtists()
   }, []);
 
-  if (loading) return <CircularProgress />;
+  if (loading || loadingArtists) return <CircularProgress />;
   if (!user) return <CircularProgress />; // fallback, AuthProvider handles redirect
   return (
-    <>
-     artists page
-    </>
+      <Grid container spacing={3} padding={2}>
+        <AddArtistButton />
+        {artists.map((artist) => (
+          <Grid
+            key={artist.id}
+            size={{ xs: 12, sm: 6, md: 4 }}
+          >
+            <Card>
+              <CardContent>
+                <Typography variant="h6">
+                  {artist.name}
+                </Typography>
+                <Button onClick={() => router.push(`/artist/${artist.id}`)}>View Artist</Button>
+                <Button onClick={() => router.push(`/artist/${artist.id}`)}>Edit Artist</Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>   
   );
 }
