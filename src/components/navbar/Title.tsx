512017e75199a4c3d0115736e9beefea6fca2845
Jordan Bradley <jordanrhysbradley@gmail.com>
2026-06-18 23:38:39 -0400

    fix for title char count

--- src/components/navbar/Title.tsx
+++ src/components/navbar/Title.tsx
@@ -1,11 +1,12 @@
 import { Typography, useMediaQuery } from "@mui/material"
 
-export default function Title({ title }: { title: String }) {
+export default function Title({ title }: { title: string }) {
   const isNotMobile = useMediaQuery("(min-width: 500px)")
+  const displayedTitle = title.length > 20 ? `${title.slice(0, 20)}..` : title;
   return (
     isNotMobile ?
     <Typography variant="h5" sx={{ textAlign: "center" }}>{title}</Typography>
     : 
-    <Typography variant="body1" sx={{ textAlign: "center" }}>{`${title.slice(0,20)}..`}</Typography>
+    <Typography variant="body1" sx={{ textAlign: "center" }}>{displayedTitle}</Typography>
   )
 }
