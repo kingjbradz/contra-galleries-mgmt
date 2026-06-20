56155aa51dd91f5f12f7ae0f98feafd50a7ab884
Jordan Bradley <jordanrhysbradley@gmail.com>
2026-06-19 23:01:26 -0400

    page header setter on artists page

--- src/app/(protected)/artists/page.tsx
+++ src/app/(protected)/artists/page.tsx
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import AddArtistForm from "@/components/artists/add/AddArtistForm";
 import ListPageActionRow from "@/components/ui/ListPageActionRow";
 import ArtistsTable from "@/components/artists/ArtistsTable";
+import { PageHeaderSetter } from "@/context/page-header/PageHeaderSetter";
 
 export interface Artist {
   id?: string;
@@ -13,6 +14,7 @@ export interface Artist {
 export default async function ArtistsPage() {
   return (
     <Grid container spacing={3} padding={2}>
+      <PageHeaderSetter title="Artists" />
       <ListPageActionRow
         label="Add Artist"
         title="Add Artist"
